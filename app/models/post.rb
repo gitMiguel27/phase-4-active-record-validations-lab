d4b4ff5ed1ec1b9b083fa3f58dde1fb455f8fe53
Miguel Nazario <mnazario35@gmail.com>
2022-04-05 12:38:20 -0400

    commit tree 811c4f27c5383216cd1a524b2e1e678165fe15b7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+
+    validate :clickbait_y
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+    
+      def clickbait_y
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "make more clickbaity!")
+        end
+      end
 end
